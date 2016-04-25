c15f716ad515fd3256340b558abf448ac66d181e
kishore-ta <kishorekumarm@techaspect.com>
2016-04-25 18:04:21 +0530

    modified

--- TAClassifieds/TAClassifieds/App_Start/Startup.Auth.cs
+++ TAClassifieds/TAClassifieds/App_Start/Startup.Auth.cs
@@ -32,7 +32,7 @@ namespace TAClassifieds
                appId: "",
                appSecret: "");
 
-            //app.UseGoogleAuthentication();
+            app.UseGoogleAuthentication();
         }
     }
 }
