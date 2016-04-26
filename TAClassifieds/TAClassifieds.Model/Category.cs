38d35ccb6d0b3aa2b1c1628a4e3b380f7bc0191e
kishore-ta <kishorekumarm@techaspect.com>
2016-04-26 17:20:24 +0530

    Data and Models

---
+++ TAClassifieds/TAClassifieds.Model/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TAClassifieds.Model
+{
+    [Table("TAC_Category")]
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryImage { get; set; }
+    }
+}
