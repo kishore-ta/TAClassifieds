6a762a67c77181bb4a259353fba332cf416fee92
kishore-ta <kishorekumarm@techaspect.com>
2016-05-03 16:45:19 +0530

    added key attribute

--- TAClassifieds/TAClassifieds.Model/ClassifiedContact.cs
+++ TAClassifieds/TAClassifieds.Model/ClassifiedContact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace TAClassifieds.Model
 {
    public class ClassifiedContact
     {
+        [Key]
         public int ContactId { get; set; }
         public string ContactName { get; set; }
         public string ContactPhone { get; set; }
